1289a6e3d9ae7801b5fe2082aa89ce4591ba03c1
Richard Scarrott <riscarrott@googlemail.com>
2020-10-21 15:27:05 +0100

    Check in dist

---
+++ dist/index.d.ts
@@ -0,0 +1,13 @@
+export { IRules, IRule } from './types'
+export { shield } from './shield'
+export {
+  rule,
+  inputRule,
+  allow,
+  deny,
+  and,
+  chain,
+  race,
+  or,
+  not,
+} from './constructors'
